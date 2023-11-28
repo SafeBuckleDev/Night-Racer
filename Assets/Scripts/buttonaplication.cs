f72d88f5b041e23123a1d1216da79f6c4f37bdee
empty71 <220006@student.glu.nl>
2023-11-28 10:28:29 +0100

    Update buttonaplication.cs

--- Assets/Scripts/buttonaplication.cs
+++ Assets/Scripts/buttonaplication.cs
@@ -8,11 +8,9 @@ public class buttonaplication : MonoBehaviour
 {
     public string url;
    
-    public string[] url4;
-   [Range(0,2)] public float urlrange;
+   
 
-    public Slider slider;
-    public Button button;
+   
 
     public void openURL()
     {
@@ -21,22 +19,5 @@ public class buttonaplication : MonoBehaviour
 
     }
 
-    private void Update()
-    {
-     
-       if(urlrange== 1)
-       {
-            url = url4[1];
-       }
-       else if(urlrange==0)
-       {
-            url = url4[0];
-       }
-       else if (urlrange == 2)
-        {
-            url = url4[2];
-        }
-
-        urlrange = slider.value;
-    }
+  
 }
